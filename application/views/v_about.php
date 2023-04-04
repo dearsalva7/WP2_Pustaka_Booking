dd9e0197eb292b539596e5c2da00ffe6e91601e4
dearsalva7 <dearsalva9@gmail.com>
2023-04-04 16:02:04 +0700

    Pustaka Booking

---
+++ application/views/v_about.php
@@ -0,0 +1,25 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Dearsalva</li>
+        <li>Nama Belakang : Sudrajat</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li>Kp.Kelapa RT 03/02 No.27 Desa Rawapanjang </li>
+        <li>Kec.Bojonggede Kab.Bogor </li>
+    </ul>
+
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Jakarta</li>
+    </ul>
+
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>Badminton</li>
+        <li>Sepakbola</li>
+    </ul>
+</section>
