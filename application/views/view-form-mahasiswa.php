4607f6980c5c3f6a731f4206e3010a75bc1770ce
dearsalva7 <dearsalva9@gmail.com>
2023-05-25 15:41:55 +0700

    Pustaka Booking

---
+++ application/views/view-form-mahasiswa.php
@@ -0,0 +1,111 @@
+<html>
+
+<head>
+    <title>Form Input Data Siswa</title>
+</head>
+<style>
+    fieldset{
+        width: 30%;
+        margin: 40px auto;
+        border-radius: 10px;
+    }
+    select{
+        width: 100%;
+    }
+</style>
+
+<body>
+    <center>
+        <fieldset>
+        <form action= "<?= base_url('mahasiswa/cetak'); ?>" method="post">
+            <table>
+                <tr>
+                    <th colspan="3">
+                        Form Input Data Siswa
+                    </th>
+                </tr>
+                <tr>
+                    <td colspan="3">
+                        <hr>
+                    </td>
+                </tr>
+                <tr>
+                    <th>Nama Lengkap</th>
+                    <th>:</th>
+                    <td>
+                        <input type="text" name="nama" id="nama">
+                    </td>
+                </tr>
+                <tr>
+                    <th>Nomor Induk Siswa</th>
+                    <td>:</td>
+                    <td>
+                        <input type="number" name="nis" id="nis">
+                    </td>
+                </tr>
+                <tr>
+                    <th>Kelas</th>
+                    <td>:</td>
+                    <td>
+                        <input type="text" name="kelas" id="kelas">
+                    </td>
+                </tr>
+                <tr>
+                    <th>Tempat Lahir</th>
+                    <td>:</td>
+                    <td>
+                        <input type="text" name="tempat" id="tempat">
+                    </td>
+                </tr>
+                <tr>
+                    <th>Tanggal Lahir</th>
+                    <td>:</td>
+                    <td>
+                        <input type="date" name="tanggal" id="tanggal">
+                    </td>
+                </tr>
+                <tr>
+                    <th>Alamat</th>
+                    <td>:</td>
+                    <td>
+                        <input type="text" name="alamat" id="alamat">
+                    </td>
+                </tr>
+                <tr>
+                    <th>Jenis Kelamin</th>
+                    <td>:</td>
+                    <td>
+                        <input type="radio" id="jekel" name="jekel" value="Laki-Laki">
+                        <label for="lakilaki">Laki-Laki</label><br>
+                        <input type="radio" id="jekel" name="jekel" value="Perempuan">
+                        <label for="perempuan">Perempuan</label><br>
+                    </td>
+                </tr>
+                <tr>
+                    <th>Agama</th>
+                    <td>:</td>
+                    <td>
+                        <select name="agama" id="agama">
+                            <option value="">Pilih Agama</option>
+                            <option value="Islam">Islam</option>
+                            <option value="Kristen">Kristen</option>
+                            <option value="Katolik">Katolik</option>
+                            <option value="Buddha">Buddha</option>
+                            <option value="Hindu">Hindu</option>
+                            <option value="Protestan">Protestan</option>
+                            <option value="Khonghucu">Khonghucu</option>
+                        </select>
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="3" align="center">
+                        <input type="submit" value="Submit">
+                    </td>
+                </tr>
+            </table>
+        </form>
+        </fieldset>
+    </center>
+</body>
+
+</html>
